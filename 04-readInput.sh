1da2fe746a63ed9f4d27621671bb116605d191fd
Srikanth <kanth01234@gmail.com>
2026-01-29 16:53:43 +0530

    script updated

--- 04-readInput.sh
+++ 04-readInput.sh
@@ -9,6 +9,19 @@ read -s PASSWORD
 
 echo "Welcome $USER_NAME to the shell scripting class"
 echo "Happy learning ......."
+
+#### Special Variables ####
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to script: $*"
+
 END_TIMEE=$(date +%s)
 TOTAL_TIME=$((END_TIMEE-START_TIMEE))
 
