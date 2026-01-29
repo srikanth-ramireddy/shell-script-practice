dd045596740e1f832008250120f062c364d6ba64
Srikanth <kanth01234@gmail.com>
2026-01-29 16:37:45 +0530

    script updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Suresh:: Hello Ramesh, How are you?"
-echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
-echo "Suresh:: doing fine. What are you learning Ramesh?"
-echo "Ramesh:: I am learning Shell. What about you?"
+PERSON1="Ramu"
+PERSON2="Ravana"
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
