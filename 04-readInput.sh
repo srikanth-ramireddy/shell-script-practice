b0e2483fdb67ac074046e596a6c631377d691f53
Srikanth <kanth01234@gmail.com>
2026-01-29 16:51:16 +0530

    script updated

--- 04-readInput.sh
+++ 04-readInput.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIMEE=$(date +%s)
 echo "Please Enter Username"
 read USER_NAME
 
@@ -8,3 +9,7 @@ read -s PASSWORD
 
 echo "Welcome $USER_NAME to the shell scripting class"
 echo "Happy learning ......."
+END_TIMEE=$(date +%s)
+TOTAL_TIME=$((END_TIMEE-START_TIMEE))
+
+echo "Total time taken to execute this script is $TOTAL_TIME seconds"
