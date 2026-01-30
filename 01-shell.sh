6c259d5a13c173fa309435628743ccfb57adfe7e
Srikanth <kanth01234@gmail.com>
2026-01-30 10:19:59 +0530

    script updated

--- 01-shell.sh
+++ 01-shell.sh
@@ -5,4 +5,13 @@ echo "This is my first shell script"
 
 VAR1="SURESH"
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+else 
+    echo "User id is $USERID"
+fi
+
 echo "Hi this ${VAR1}" 
